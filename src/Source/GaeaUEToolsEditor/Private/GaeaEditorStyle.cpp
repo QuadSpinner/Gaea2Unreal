97b7295eedcefb3e43f77e47575e1b9efacf5804
Dax Pandhi <dax@quadspinner.com>
2025-03-24 14:16:36 +0530

    Plugin directory warning added.

--- src/Source/GaeaUEToolsEditor/Private/GaeaEditorStyle.cpp
+++ src/Source/GaeaUEToolsEditor/Private/GaeaEditorStyle.cpp
@@ -43,9 +43,9 @@ void FGaeaEditorStyle::Initialize()
 	else
 	{
 		// Plugin not found
-		UE_LOG(LogTemp, Error, TEXT("GaeaUnrealTools plugin not found"));
-	}
-	
+		UE_LOG(LogTemp, Error, TEXT("GaeaUnrealTools icon path not found. This is likely caused by the plugin being installed in an incorrect location or folder name. Please see https://gaea.app/uedocs for exact instructions for the file path."));
+	}	
+
  
 	// Register the style set
 	FSlateStyleRegistry::RegisterSlateStyle(*StyleSet.Get());
